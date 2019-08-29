445311942db9bad4943d4dc6cee81f0fa1c98c02
huolongshen <635360172@qq,com>
2019-08-29 21:28:27 +0800

    第二个

--- js/functions.js
+++ js/functions.js
@@ -134,3 +134,14 @@ function adjustCodePosition() {
 function showLoveU() {
 	$('#loveu').fadeIn(3000);
 }
+
+function toggleSound() {
+    var music = document.getElementById("bgmMusic");//获取ID  
+    //console.log(music);
+    //console.log(music.paused);
+    if (music.paused) { //判读是否播放  
+         music.paused=false;
+         music.play(); //没有就播放 
+     }  
+            
+}
